1e68295fbc2ff6f4f1f5a8eab9a9e3491a5628d5
AlexandreDev06 <alexandre.macedo1712@gmail.com>
2021-11-03 22:00:24 -0300

    texto piscar na tela corrigido

--- script.js
+++ script.js
@@ -5,26 +5,22 @@ var num = 0;
 
 function next() {
   num += 1;
-
   if (num == 1) {
-    text.style.animation = "blinkCursor 800ms steps(40) infinite normal";
-    setTimeout(() => text.style.animation = "blinkCursor 800ms steps(40) infinite normal, typing2 4s steps(40) 1s normal both", 5);
+    text.style.animation = "blinkCursor 800ms steps(40) infinite normal, typing2 4s steps(40) 500ms normal both";
     text.innerHTML = "Prazer em conheçe-lo, quero que veja oque posso fazer";
   } else if (num == 2) {
-    text.style.animation = "blinkCursor 800ms steps(40) infinite normal";
-    setTimeout(() => text.style.animation = "blinkCursor 800ms steps(40) infinite normal, typing3 5s steps(40) 1s normal both", 5);
+    text.style.animation = "blinkCursor 800ms steps(40) infinite normal, typing3 5s steps(50) 500ms normal both";
     text.innerHTML = "Talvez esse programa não seja tão dificil de fazer como voce imagina!";
   } else if (num == 3) {
-    text.style.animation = "blinkCursor 800ms steps(40) infinite normal";
-    setTimeout(() => text.style.animation = "blinkCursor 800ms steps(40) infinite normal, typing4 4s steps(40) 1s normal both", 5);
+    text.style.animation = "blinkCursor 800ms steps(40) infinite normal, typing4 4s steps(40) 500ms normal both";
     text.innerHTML = "Posso te apresentar meu projeto?";
   } else if (num == 4) {
-    text.className = "animate__animated animate__fadeIn";
+    text.className = "animate__animated animate__fadeIn animate__slow";
     text.style = '';
     text.innerHTML = "<p style='color: white;'>Esse é meu projeto chamado My Finances</p> <br><img src='myfinances.png' height='700' width='1200' border='1px'>";
   }
-  if (num > 4) {
-    text.innerHTML = "Olá, meu nome é Alexandre";
+  if (num > 4 || num <= 0) {
+    text.innerHTML = "Olá meu nome é Alexandre =)";
     num = 0;
   }
 }
